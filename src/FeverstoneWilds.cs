7eb3fed98d04952fcc0d36dc9d1a34697f0fc818
Ahrondus <62456961+Ahrondus@users.noreply.github.com>
2023-04-17 05:43:22 -0400

    ModConfig Upload
    Working version of mod config, release-ready for 1.18.

---
+++ src/FeverstoneWilds.cs
@@ -0,0 +1,16 @@
+using Vintagestory.API.Common;
+using FeverstoneWilds.Config;
+
+namespace FeverstoneWilds
+{
+	public class FWildsCore : ModSystem
+	{
+		public override void Start(ICoreAPI api)
+		{
+			base.Start(api);
+
+			ModConfig.ReadConfig(api);
+			api.World.Logger.Event("Reading 'FeverstoneWilds' Config");
+		}
+	}
+}
