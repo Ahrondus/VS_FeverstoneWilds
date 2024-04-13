f2cd9a65f919f68ea6b65405ea18664a4a358f3a
Ahrondus <62456961+Ahrondus@users.noreply.github.com>
2024-04-13 16:48:34 -0500

    rc.5 updates
    Added hunter armor set, attempt to establish a single largenest item to be compatible with cockatrice and ostrich, and update descriptions based on which bird is using the nest

--- feverstonewilds/src/FeverstoneWilds.cs
+++ feverstonewilds/src/FeverstoneWilds.cs
@@ -11,9 +11,11 @@ namespace FeverstoneWilds
 
 			api.RegisterBlockClass("BlockAnimalNestCockatrice", typeof(BlockAnimalNest));
 			api.RegisterBlockClass("BlockAnimalNestOstrich", typeof(BlockAnimalNest));
+			api.RegisterBlockClass("BlockAnimalNestLarge", typeof(BlockAnimalNest));
 
 			api.RegisterBlockEntityClass("AnimalNestOstrich", typeof(BlockEntityAnimalNestOstrich));
             api.RegisterBlockEntityClass("AnimalNestCockatrice", typeof(BlockEntityAnimalNestCockatrice));
+			api.RegisterBlockEntityClass("AnimalNestLarge", typeof (BlockEntityAnimalNestLarge));
 
             ModConfig.ReadConfig(api);
 		}
