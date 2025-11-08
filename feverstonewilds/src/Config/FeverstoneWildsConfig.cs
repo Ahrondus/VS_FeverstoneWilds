1596ab92b8571f61395ca6416ab949614e01c326
Ahrondus <62456961+Ahrondus@users.noreply.github.com>
2025-11-08 00:22:11 -0600

    2.0.0-rc.7
    Added animalcages compatibility, added configlib compatibility, updated modinfo to rc.7, updated lang files, updated spawn rates for many creatures

--- feverstonewilds/src/Config/FeverstoneWildsConfig.cs
+++ feverstonewilds/src/Config/FeverstoneWildsConfig.cs
@@ -36,12 +36,61 @@ namespace FeverstoneWilds.Config
         public bool FSWOrcaEnabled = true;
         public bool FSWSharksEnabled = true;
         public bool FSWStingrayEnabled = true;
+        public bool FSWGhostfishEnabled = true;
 
         // Golem Types
         public string eachgolem = "----- Golems by Type -----";
         public bool FSWCopperGolemEnabled = true;
         public bool FSWTinGolemEnabled = true;
         public bool FSWIronGolemEnabled = true;
+
+        // Animal Cages Config
+        public string animalcages = "----- Animal Cages Config -----";
+        public bool AllowCagedFSWCreatures = true;
+
+        // ConfigLib Config
+
+        public string configlibhints = "----- Config Lib Spawn Rates -----";
+        public float BISON_SPAWN_CHANCE_WORLDGEN { get { return _bison_spawn_worldgen; } set {_bison_spawn_worldgen = value >= 0 ? (value*0.0005f) : 0;} }
+        private float _bison_spawn_worldgen = 0.002f;
+
+        public float BISON_SPAWN_CHANCE_RUNTIME { get { return _bison_spawn_runtime; } set { _bison_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _bison_spawn_runtime = 0.00003f;
+        public float COCKATRICE_SPAWN_CHANCE_WORLDGEN { get { return _cockatrice_spawn_worldgen; } set {_cockatrice_spawn_worldgen = value >= 0 ? (value*0.001f) : 0;} }
+        private float _cockatrice_spawn_worldgen = 0.004f;
+
+        public float COCKATRICE_SPAWN_CHANCE_RUNTIME { get { return _cockatrice_spawn_runtime; } set { _cockatrice_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _cockatrice_spawn_runtime = 0.00003f;
+        public float DIREWOLF_SPAWN_CHANCE_WORLDGEN { get { return _direwolf_spawn_worldgen; } set {_direwolf_spawn_worldgen = value >= 0 ? (value*0.0025f) : 0;} }
+        private float _direwolf_spawn_worldgen = 0.010f;
+
+        public float DIREWOLF_SPAWN_CHANCE_RUNTIME { get { return _direwolf_spawn_runtime; } set { _direwolf_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _direwolf_spawn_runtime = 0.00004f;
+        public float FAUNLING_SPAWN_CHANCE_WORLDGEN { get { return _faunling_spawn_worldgen; } set {_faunling_spawn_worldgen = value >= 0 ? (value*0.002375f) : 0;} }
+        private float _faunling_spawn_worldgen = 0.0095f;
+
+        public float FAUNLING_SPAWN_CHANCE_RUNTIME { get { return _faunling_spawn_runtime; } set { _faunling_spawn_runtime = value >= 0 ? (value*0.000005f) : 0; } }
+        private float _faunling_spawn_runtime = 0.00002f;
+        public float GOLEM_SPAWN_CHANCE_WORLDGEN { get { return _golem_spawn_worldgen; } set {_golem_spawn_worldgen = value >= 0 ? (value*0.002f) : 0;} }
+        private float _golem_spawn_worldgen = 0.006f;
+
+        public float GOLEM_SPAWN_CHANCE_RUNTIME { get { return _golem_spawn_runtime; } set { _golem_spawn_runtime = value >= 0 ? (value*0.000005f) : 0; } }
+        private float _golem_spawn_runtime = 0.00001f;
+        public float HELLBOAR_SPAWN_CHANCE_WORLDGEN { get { return _hellboar_spawn_worldgen; } set {_hellboar_spawn_worldgen = value >= 0 ? (value*0.0025f) : 0;} }
+        private float _hellboar_spawn_worldgen = 0.010f;
+
+        public float HELLBOAR_SPAWN_CHANCE_RUNTIME { get { return _hellboar_spawn_runtime; } set { _hellboar_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _hellboar_spawn_runtime = 0.00004f;
+        public float HORSE_SPAWN_CHANCE_WORLDGEN { get { return _horse_spawn_worldgen; } set {_horse_spawn_worldgen = value >= 0 ? (value*0.0025f) : 0;} }
+        private float _horse_spawn_worldgen = 0.010f;
+
+        public float HORSE_SPAWN_CHANCE_RUNTIME { get { return _horse_spawn_runtime; } set { _horse_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _horse_spawn_runtime = 0.00003f;
+        public float OSTRICH_SPAWN_CHANCE_WORLDGEN { get { return _ostrich_spawn_worldgen; } set {_ostrich_spawn_worldgen = value >= 0 ? (value*0.00125f) : 0;} }
+        private float _ostrich_spawn_worldgen = 0.005f;
+
+        public float OSTRICH_SPAWN_CHANCE_RUNTIME { get { return _ostrich_spawn_runtime; } set { _ostrich_spawn_runtime = value >= 0 ? (value*0.00001f) : 0; } }
+        private float _ostrich_spawn_runtime = 0.00002f;
 
         public FeverstoneWildsConfig() { }
 
@@ -75,11 +124,33 @@ namespace FeverstoneWilds.Config
             FSWOrcaEnabled = previousConfig.FSWOrcaEnabled;
             FSWSharksEnabled = previousConfig.FSWSharksEnabled;
             FSWStingrayEnabled = previousConfig.FSWStingrayEnabled;
+            FSWGhostfishEnabled = previousConfig.FSWGhostfishEnabled;
 
             // Golem Types
             FSWCopperGolemEnabled = previousConfig.FSWCopperGolemEnabled;
             FSWTinGolemEnabled = previousConfig.FSWTinGolemEnabled;
             FSWIronGolemEnabled = previousConfig.FSWIronGolemEnabled;
+
+            // AnimalCages Config
+            AllowCagedFSWCreatures = previousConfig.AllowCagedFSWCreatures;
+
+            // ConfigLib Configs
+            BISON_SPAWN_CHANCE_WORLDGEN = previousConfig.BISON_SPAWN_CHANCE_WORLDGEN;
+            BISON_SPAWN_CHANCE_RUNTIME = previousConfig.BISON_SPAWN_CHANCE_RUNTIME;
+            COCKATRICE_SPAWN_CHANCE_WORLDGEN = previousConfig.COCKATRICE_SPAWN_CHANCE_WORLDGEN;
+            COCKATRICE_SPAWN_CHANCE_RUNTIME = previousConfig.COCKATRICE_SPAWN_CHANCE_RUNTIME;
+            DIREWOLF_SPAWN_CHANCE_WORLDGEN = previousConfig.DIREWOLF_SPAWN_CHANCE_WORLDGEN;
+            DIREWOLF_SPAWN_CHANCE_RUNTIME = previousConfig.DIREWOLF_SPAWN_CHANCE_RUNTIME;
+            FAUNLING_SPAWN_CHANCE_WORLDGEN = previousConfig.FAUNLING_SPAWN_CHANCE_WORLDGEN;
+            FAUNLING_SPAWN_CHANCE_RUNTIME = previousConfig.FAUNLING_SPAWN_CHANCE_RUNTIME;
+            GOLEM_SPAWN_CHANCE_WORLDGEN = previousConfig.GOLEM_SPAWN_CHANCE_WORLDGEN;
+            GOLEM_SPAWN_CHANCE_RUNTIME = previousConfig.GOLEM_SPAWN_CHANCE_RUNTIME;
+            HELLBOAR_SPAWN_CHANCE_WORLDGEN = previousConfig.HELLBOAR_SPAWN_CHANCE_WORLDGEN;
+            HELLBOAR_SPAWN_CHANCE_RUNTIME = previousConfig.HELLBOAR_SPAWN_CHANCE_RUNTIME;
+            HORSE_SPAWN_CHANCE_WORLDGEN = previousConfig.HORSE_SPAWN_CHANCE_WORLDGEN;
+            HORSE_SPAWN_CHANCE_RUNTIME = previousConfig.HORSE_SPAWN_CHANCE_RUNTIME;
+            OSTRICH_SPAWN_CHANCE_WORLDGEN = previousConfig.OSTRICH_SPAWN_CHANCE_WORLDGEN;
+            OSTRICH_SPAWN_CHANCE_RUNTIME = previousConfig.OSTRICH_SPAWN_CHANCE_RUNTIME;
         }
     }
 }
